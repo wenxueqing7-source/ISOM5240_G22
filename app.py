d8110b43d486d023c69d1d6ecce2fc38f5200cb7
MelodyWEN <wenxueqing7@gmail.com>
2026-05-05 14:56:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if uploaded_file is not None:
 
     # Stage 2: Text to Story (Inline)
     st.text('Generating a story...')
-    story_pipe = pipeline("text-generation", model="ajibawa-2023/Young-Children-Storyteller-Mistral-7B")
+    story_pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
     story_results = story_pipe(scenario)
     story = story_results[0]['generated_text']
     st.write(f"**Story:** {story}")
